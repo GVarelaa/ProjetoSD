58c8aa6095e117b8b13bb7e848bf76140ebc2696
gabrielasfc <gsfcunha@gmail.com>
2022-12-02 23:48:58 +0000

    adicionei a classe position

---
+++ src/Position.java
@@ -0,0 +1,57 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Position(Position pos) {
+        this.x = pos.getX();
+        this.y = pos.getY();
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append("(").append(this.x);
+        sb.append(",").append(this.y);
+        sb.append(")");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position that = (Position) o;
+        return this.x == that.getX() && this.y == that.getY();
+    }
+
+    @Override
+    public Position clone() {
+        return new Position(this);
+    }
+}
