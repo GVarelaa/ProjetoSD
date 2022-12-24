fc291a12c2f775aac46025fb441b892a72357f0b
gabrielasfc <gsfcunha@gmail.com>
2022-12-24 14:29:27 +0000

    client menu for register/login

--- src/RunClient.java
+++ src/RunClient.java
@@ -1,11 +1,32 @@
 import SharedState.Position;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class RunClient {
     public static void main(String[] args) throws IOException, InterruptedException {
         Client client = new Client();
         client.start();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("============");
+        System.out.println("1... Sign up");
+        System.out.println("2... Sign in");
+        System.out.println("============");
+
+        int opt = sc.nextInt();
+
+        if (opt == 1 || opt == 2) {
+            System.out.println("Introduza o username: ");
+            String username = sc.next();
+            System.out.println("Introduza a password: ");
+            String password = sc.next();
+
+            if (opt == 1) client.register(username, password);
+            else client.login(username, password);
+        }
+
 
         //String username = "guilherme";
         //String password = "lol";
