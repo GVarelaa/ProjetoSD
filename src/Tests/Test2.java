bc82feb1a1641c778f04b8b4b68dcbcda6d9ae9a
MiguelBraga2 <mgdsbraga@gmail.com>
2023-01-08 17:03:21 +0000

    Added test2

---
+++ src/Tests/Test2.java
@@ -0,0 +1,42 @@
+package Tests;
+
+import Exceptions.NonExistentUsernameException;
+import Exceptions.UsernameAlreadyExistsException;
+import SharedState.Position;
+import SharedState.Reservation;
+import SharedState.ScooterManagerImpl;
+
+public class Test2 {
+    public static void main(String[] args) throws UsernameAlreadyExistsException, NonExistentUsernameException, InterruptedException {
+        ScooterManagerImpl sm = new ScooterManagerImpl(20, 20, 10);
+        sm.register("miguel", "mike");
+        sm.login("miguel", "mike");
+
+        int threadNum = 10;
+        Thread t[] = new Thread[threadNum];
+
+        for (int i=0; i<threadNum; i++){
+            t[i] = new Thread(() -> {
+                Reservation r=null;
+                try{
+                    r = sm.activateScooter(new Position(5,5), "miguel");
+                } catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+                if (r != null) System.out.println(r.toString());
+
+                System.out.println(sm.parkScooter(r.getReservationID(), new Position(1,1)) + " €");
+            });
+            t[i].start();
+            Thread.sleep(10);
+        }
+
+        for(int i=0; i<threadNum; i++){
+            t[i].join();
+        }
+
+        System.out.println("Terminei");
+
+
+    }
+}
